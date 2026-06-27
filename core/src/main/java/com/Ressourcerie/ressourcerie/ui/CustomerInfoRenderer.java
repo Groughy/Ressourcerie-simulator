5a0de354a17352b0f3d3a9e3e3c077b3a48f0e30
Groughy <lecoinpanda@gmail.com>
2026-06-27 16:24:59 +0200

    Ajout CustomerInfoRenderer

---
+++ core/src/main/java/com/Ressourcerie/ressourcerie/ui/CustomerInfoRenderer.java
@@ -0,0 +1,20 @@
+package com.Ressourcerie.ressourcerie.ui;
+
+import com.Ressourcerie.ressourcerie.customer.Customer;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+
+public class CustomerInfoRenderer {
+
+    public void render(SpriteBatch batch, BitmapFont font, Customer customer){
+        if (customer == null) {
+            return;
+        }
+
+        font.draw(batch, "Client : " + customer.name, 40, 120);
+        font.draw(batch, "Budget : " + customer.budget + " euros", 40, 90);
+        font.draw(batch, "Recherche : " + customer.wantedItems, 40, 60);
+        font.draw(batch, "Type : " + customer.customerType, 40, 30);
+    }
+
+}
