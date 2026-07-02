8298ec526100e4e8af03c0ce67917e724a57e5c6
Groughy <lecoinpanda@gmail.com>
2026-07-02 22:03:09 +0200

    Préparation ajout icone

--- core/src/main/java/com/Ressourcerie/ressourcerie/Assets.java
+++ core/src/main/java/com/Ressourcerie/ressourcerie/Assets.java
@@ -10,18 +10,34 @@ public class Assets {
     public static Texture topbarTexture;
     public static Texture messagebarTexture;
 
+    public static Texture iconElectronic;
+    public static Texture iconMechanic;
+    public static Texture iconFurniture;
+    public static Texture iconDecoration;
+    public static Texture iconTextile;
+
     public static void load() {
         backgroundTexture = new Texture("ui/background.png");
         panelLeftTexture = new Texture("ui/panel_left.png");
         panelRightTexture = new Texture("ui/panel_right.png");
         topbarTexture = new Texture("ui/topbar.png");
         messagebarTexture = new Texture("ui/messagebar.png");
+        iconElectronic = new Texture("ui/icon_electronic.png");
+        iconMechanic = new Texture("ui/icon_mechanic.png");
+        iconFurniture = new Texture("ui/icon_furniture.png");
+        iconDecoration = new Texture("ui/icon_decoration.png");
+        iconTextile = new Texture("ui/icon_textile.png");
     }
 
     public static void dispose() {
         backgroundTexture.dispose();
         panelLeftTexture.dispose();
         panelRightTexture.dispose();
+        iconElectronic.dispose();
+        iconMechanic.dispose();
+        iconFurniture.dispose();
+        iconDecoration.dispose();
+        iconTextile.dispose();
 
         if (topbarTexture != null) {
             topbarTexture.dispose();
