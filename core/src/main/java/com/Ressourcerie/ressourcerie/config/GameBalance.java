fdf1d92eb14c3ac6f1709d059ae342302086b170
Groughy <sebastien.journiac@orange.fr>
2026-07-16 10:31:50 +0200

    Ajout du GameBalance file

---
+++ core/src/main/java/com/Ressourcerie/ressourcerie/config/GameBalance.java
@@ -0,0 +1,62 @@
+package com.Ressourcerie.ressourcerie.config;
+
+public final class GameBalance {
+
+    private GameBalance() {
+    }
+
+    public static final int STARTING_MONEY = 100;
+    public static final int MAX_ENERGY = 100;
+
+    public static final int MIN_REPAIR_COST = 1;
+    public static final int BASE_REPAIR_AMOUNT = 5;
+    public static final int REPAIR_AMOUNT_PER_WORKSHOP_LEVEL = 5;
+
+    public static final int COFFEE_COST = 10;
+    public static final int COFFEE_ENERGY_BOOST = 20;
+
+    public static final int REPAIR_KIT_COST = 40;
+    public static final int REPAIR_KIT_BONUS = 5;
+
+    public static final int EMPLOYEE_RECRUIT_COST = 200;
+    public static final int EMPLOYEE_BASE_SALARY = 20;
+    public static final int EMPLOYEE_TRAINING_COST = 100;
+
+    public static final int EMPLOYEE_BASE_REPAIR_POWER = 5;
+    public static final int EMPLOYEE_REPAIR_POWER_PER_LEVEL = 5;
+
+    public static final int EMPLOYEE_BASE_FAIL_CHANCE = 20;
+    public static final int EMPLOYEE_FAIL_REDUCTION_PER_LEVEL = 3;
+    public static final int EMPLOYEE_MIN_FAIL_CHANCE = 5;
+    public static final int EMPLOYEE_FAILURE_DAMAGE = 5;
+
+    public static final int STARTING_INVENTORY_CAPACITY = 10;
+    public static final int STARTING_SELLING_CAPACITY = 10;
+
+    public static final int STORAGE_UPGRADE_COST = 120;
+    public static final int STORAGE_CAPACITY_GAIN = 5;
+
+    public static final int WORKSHOP_BASE_UPGRADE_COST = 100;
+    public static final int MAX_WORKSHOP_LEVEL = 10;
+
+    public static final int MIN_CUSTOMER_BUDGET = 50;
+    public static final int CUSTOMER_BUDGET_RANGE = 201;
+
+    public static final int STARTING_REPUTATION = 50;
+    
+    public static final int HAPPY_REPUTATION_GAIN = 5;
+    public static final int UNHAPPY_REPUTATION_LOSS = 5;
+
+    public static final int BAD_REPUTATION_THRESHOLD = 30;
+    public static final int GOOD_REPUTATION_THRESHOLD = 70;
+    public static final int EXCELLENT_REPUTATION_THRESHOLD = 90;
+
+    public static final int MIN_NEW_ITEMS_PER_DAY = 2;
+    public static final int NEW_ITEMS_RANDOM_RANGE = 3;
+
+    public static final float MAX_ACCEPTABLE_PRICE_MULTIPLIER = 1.5f;
+    public static final float GOOD_DEAL_PRICE_MULTIPLIER = 0.8f;
+
+    public static final int COLLECTER_BONUS = 20;
+
+}
